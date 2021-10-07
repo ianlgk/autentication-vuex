5426672f171ab058c4b4b23c58ac3514c6404635
Ian Langkammer Batista <ilangkammer16@gmail.com>
2021-10-07 17:02:05 -0300

    feat: user can logout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,12 +15,32 @@
               Gerentes
             </router-link>
           </li>
+          <li>
+            <a
+              href="#"
+              class="nav-link"
+              @click.prevent="logout()"
+            >
+              Logout
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      localStorage.removeItem('token');
+      this.$router.push({ name: 'login' });
+    }
+  }
+}
+</script>
 
 <style>
 .navbar {
